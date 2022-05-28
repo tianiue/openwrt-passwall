ea763c5d79884240291632cc8c8567926ed74f69
tianiue <61725402+tianiue@users.noreply.github.com>
2022-05-28 19:54:28 +0800

    luci: update default rules

--- luci-app-passwall/luasrc/controller/passwall.lua
+++ luci-app-passwall/luasrc/controller/passwall.lua
@@ -181,7 +181,7 @@ end
 function status()
 	-- local dns_mode = ucic:get(appname, "@global[0]", "dns_mode")
 	local e = {}
-	e.dns_mode_status = luci.sys.call("netstat -apn | grep ':15353 ' >/dev/null") == 0
+	e.dns_mode_status = luci.sys.call("netstat -apn | grep ':5353 ' >/dev/null") == 0
 	e.haproxy_status = luci.sys.call(string.format("top -bn1 | grep -v grep | grep '%s/bin/' | grep haproxy >/dev/null", appname)) == 0
 	e["tcp_node_status"] = luci.sys.call(string.format("top -bn1 | grep -v -E 'grep|acl/|acl_' | grep '%s/bin/' | grep -i 'TCP' >/dev/null", appname)) == 0
 
@@ -488,4 +488,3 @@ function hysteria_update()
 
 	http_write_json(json)
 end
-
